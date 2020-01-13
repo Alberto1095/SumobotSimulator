7f523c5721d95a4e4f9ef57421e38d83205418ba
Alberto <albertomon.333@gmail.com>
2020-01-13 23:57:38 +0100

    Added first version of robot and combat area

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 velocity = new Vector2();
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity = new Vector2(0, 1);            
+           
+        }else if(Input.GetKey(KeyCode.S))
+        {
+            velocity = new Vector2(0, -1);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            velocity = new Vector2(-1, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            velocity = new Vector2(1, 0);
+        }
+
+        rigidbody.MovePosition(rigidbody.position + velocity * speed * Time.fixedDeltaTime);
+    }
+
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {       
+        Debug.Log("COLISION ENTER: " + collision.gameObject.name );        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("COLISION EXIT: " + collision.gameObject.name);
+    }
+
+}
