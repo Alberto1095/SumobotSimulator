92d618fcb96953575cbece0bad8ae650b55ae715
alberto <jmonasterio@opendeusto.es>
2020-01-14 12:20:35 +0100

    Added line sensor

---
+++ Assets/Scripts/LineSensor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSensor : MonoBehaviour
+{   
+  
+    public Transform parentTransform;
+    public float distance;
+
+    public bool Detect(int direction)
+    {      
+        Vector2 directionVector = GetFacingVector() * direction;        
+        int layerMask = LayerMask.GetMask("Interior", "Exterior");
+        Vector2 origin;
+        Vector2 endPoint;
+
+        origin = transform.position;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(origin, directionVector,
+            distance, layerMask);
+        endPoint = origin + directionVector * direction*distance;
+        Debug.DrawLine(new Vector3(origin.x, origin.y), new Vector3(endPoint.x, endPoint.y),
+            Color.green, 0, true);        
+        bool detected = DetectOuterRing(hits);
+        
+        return detected;
+    }
+
+    private Vector2 GetFacingVector()
+    {
+        Vector2 forwardVector = new Vector2(transform.up.x, transform.up.y);
+        return forwardVector;
+    }
+
+    private bool DetectOuterRing(RaycastHit2D[] hits)
+    {
+
+        bool detectedInnerRing = false;
+        bool detectedOuterRing = false;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Interior"))
+            {
+                detectedInnerRing = true;
+            }
+            else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Exterior"))
+            {
+                detectedOuterRing = true;
+            }
+        }
+
+        return detectedOuterRing && !detectedInnerRing;
+    }
+}
--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -5,40 +5,89 @@ using UnityEngine;
 public class RobotController : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
-    public float speed;
+    public float moveSpeed;
+    public float rotationSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    public enum RobotDirection { Up, Down, Left, Right,Stop };
+    private RobotDirection currentDirection;
+
+    public LineSensor frontRightLineSensor;
+    public LineSensor frontLeftLineSensor;
+    public LineSensor backLineSensor;    
+    
+
+    private void Update()
     {
-        Debug.Log("START");
+        CheckUserInput();
+        CheckLineSensors();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 velocity = new Vector2();
+        Move();        
+    }
+
+    private void CheckUserInput()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            velocity = new Vector2(0, 1);            
-           
-        }else if(Input.GetKey(KeyCode.S))
+            currentDirection = RobotDirection.Up;
+        }
+        else if (Input.GetKey(KeyCode.S))
         {
-            velocity = new Vector2(0, -1);
+            currentDirection = RobotDirection.Down;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            velocity = new Vector2(-1, 0);
+            currentDirection = RobotDirection.Left;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            velocity = new Vector2(1, 0);
+            currentDirection = RobotDirection.Right;
         }
-
-        rigidbody.MovePosition(rigidbody.position + velocity * speed * Time.fixedDeltaTime);
+        else
+        {
+            currentDirection = RobotDirection.Stop;
+        }
     }
 
+    private void Move()
+    {
+        switch (currentDirection)
+        {
+            case RobotDirection.Up:
+                Debug.Log("MOVE UP");
+                rigidbody.MovePosition(rigidbody.position + 
+                    GetCurrentVelocity() * moveSpeed * Time.fixedDeltaTime);
+                break;
+            case RobotDirection.Down:
+                rigidbody.MovePosition(rigidbody.position +
+                   -1*GetCurrentVelocity() * moveSpeed * Time.fixedDeltaTime);
+                break;
+            case RobotDirection.Left:
+                rigidbody.MoveRotation(rigidbody.rotation + rotationSpeed * Time.fixedDeltaTime);
+                break;
+            case RobotDirection.Right:
+                rigidbody.MoveRotation(rigidbody.rotation -rotationSpeed * Time.fixedDeltaTime);
+                break;
+            case RobotDirection.Stop:
+                break;
+        }       
+    }
 
+    private Vector2 GetCurrentVelocity()
+    {
+        Vector2 forwardVector = new Vector2(transform.up.x, transform.up.y);        
+        return forwardVector;
+    }    
 
+    private void CheckLineSensors()
+    {
+        bool detected  = frontRightLineSensor.Detect(1);
+        Debug.Log("DETECTED FRL: " + detected);
+        
+    }     
 
 
     private void OnTriggerEnter2D(Collider2D collision)
