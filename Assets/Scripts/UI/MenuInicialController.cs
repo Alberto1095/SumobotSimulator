dc09ca141a0e42d5af8d685454e091962c399c71
Alberto <albertomon.333@gmail.com>
2020-01-15 19:06:02 +0100

    Singleton menu inicial added

--- Assets/Scripts/UI/MenuInicialController.cs
+++ Assets/Scripts/UI/MenuInicialController.cs
@@ -8,6 +8,25 @@ public class MenuInicialController : MonoBehaviour
 
     public static MenuInicialController Instance = null;
 
+    // Initialize the singleton instance.
+    private void Awake()
+    {
+
+        // If there is not already an instance of SoundManager, set it to this.
+        if (Instance == null)
+        {
+            Instance = this;          
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Dont destroy on load
+        DontDestroyOnLoad(gameObject);
+    }
+
 
     public void Show(bool b)
     {
