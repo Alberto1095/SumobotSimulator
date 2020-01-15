eaa4ac1de0580b627d3c8feb030fd58b496a33a1
Alberto <albertomon.333@gmail.com>
2020-01-15 19:02:46 +0100

    Added start menu

---
+++ Assets/Scripts/UI/MenuInicialController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInicialController : MonoBehaviour
+{
+    public GameObject panel;
+
+    public static MenuInicialController Instance = null;
+
+
+    public void Show(bool b)
+    {
+        panel.SetActive(b);
+    }
+
+    public void OnTrainingButtonPressed()
+    {
+        //TODO
+    }
+
+    public void OnCombatButtonPressed()
+    {
+        //TODO
+    }
+}
